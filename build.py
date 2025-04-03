d985d848922ada68a87a145af9506d6846ce81c1
Niranjan <sadulaniranjanreddy@gmail.com>
2025-04-03 18:59:58 -0500

    add css and js to static

--- build.py
+++ build.py
@@ -1,5 +1,5 @@
 import os
-from flask import render_template
+from flask import render_template, url_for
 from app import create_app
 from app.config import PORTFOLIO_CONFIG
 
@@ -10,6 +10,14 @@ def build_static_site():
     # Create a test request context
     with app.test_request_context():
         with app.app_context():
+            # Override url_for to use relative paths
+            def relative_url_for(endpoint, **values):
+                if endpoint == 'static':
+                    return f"static/{values['filename']}"
+                return url_for(endpoint, **values)
+            
+            app.jinja_env.globals['url_for'] = relative_url_for
+            
             # Render the template with the config data
             rendered = render_template('index.html', data=PORTFOLIO_CONFIG)
             
